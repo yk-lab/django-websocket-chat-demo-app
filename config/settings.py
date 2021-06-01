7fb69a0306df81f331dcc420f0ba3aa1b3e765b6
k.yamada <yetanother.yk@gmail.com>
2021-06-02 00:37:44 +0900

    使用しないファイルを削除

--- config/settings.py
+++ config/settings.py
@@ -146,9 +146,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
